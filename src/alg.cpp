eb7b3905885ddb8635aceebc20dc66a870791f17
daryalevina <78952508+daryalevina@users.noreply.github.com>
2021-06-06 21:51:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,5 @@
 // Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
+  int cbinsearch(int *arr, int size, int value) {
   int z = 0;
   int a = 0;
   int b = size - 1;
@@ -10,14 +9,14 @@ int cbinsearch(int *arr, int size, int value) {
       for (int i = 0; i < size; i++) {
         if (arr[i] == value) {
           z++;
-	}
-      }
-    break;
-    }  
+    }
+  }
+  break;
+    }
     if (arr[k] < value)
       a = k + 1;
     else
       b = k - 1;
-  } 
+  }
   return z;
 }
